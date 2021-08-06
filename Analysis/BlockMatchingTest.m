5494d2eb75c2c03af9392e77287d81e3e0bc0536
roy-hachnochi <roy.hachnochi@gmail.com>
2021-08-06 07:42:21 +0300

    Add distance prints to BM test

--- Analysis/BlockMatchingTest.m
+++ Analysis/BlockMatchingTest.m
@@ -46,11 +46,12 @@ subplot(3, nPatchesToPlot, ceil(nPatchesToPlot/2));
 refRow =   mGroupIndices(1, 1, 1);
 refCol =   mGroupIndices(1, 1, 2);
 refFrame = mGroupIndices(1, 1, 3);
-imshow(uint8(mX(refRow + (0:(p-1)), refCol + (0:(p-1)), refFrame)));
+mRefPatch = mX(refRow + (0:(p-1)), refCol + (0:(p-1)), refFrame);
+imshow(uint8(mRefPatch));
 title('Reference Frame');
 for iPlot = 1:2*nPatchesToPlot
     if iPlot <= nPatchesToPlot % show nearest patches
-        iPatch = iPlot;
+        iPatch = iPlot + 1;
         sTitle = 'Nearest Frames in Group';
     else % show furthest patches
         iPatch = vNumNeighbors(1) - (nPatchesToPlot*2 - iPlot);
@@ -60,8 +61,27 @@ for iPlot = 1:2*nPatchesToPlot
     col =   mGroupIndices(1, iPatch, 2);
     frame = mGroupIndices(1, iPatch, 3);
     subplot(3, nPatchesToPlot, nPatchesToPlot + iPlot);
-    imshow(uint8(mX(row + (0:(p-1)), col + (0:(p-1)), frame)));
+    mPatch = mX(row + (0:(p-1)), col + (0:(p-1)), frame);
+    dist = PatchesNorm(reshape(mPatch - mRefPatch, [1, p, p]), sConfig);
+    imshow(uint8(mPatch));
     if (mod(iPlot, nPatchesToPlot) == ceil(nPatchesToPlot/2))
-        title(sTitle);
+        title([sTitle,newline,sprintf('d = %.2f',dist)]);
+    else
+        title(sprintf('d = %.2f',dist));
     end
+end
+
+
+%% ==========================================================================================================
+function d = PatchesNorm(mDiffPatches, sConfig)
+
+switch sConfig.sBlockMatching.metric
+    case 'l1'
+        d = mean(abs(mDiffPatches), [2, 3]);
+    case 'l2'
+        d = sqrt(mean(abs(mDiffPatches).^2, [2, 3]));
+    otherwise
+        error('Metric not defined');
+end
+
 end
