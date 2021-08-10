7b968d26b7ac2608da2c1dbdb3d9c84b02a7ef91
roy-hachnochi <roy.hachnochi@gmail.com>
2021-08-10 10:01:20 +0300

    Chnage summary order

--- Analysis/WriteLogs2Excel.m
+++ Analysis/WriteLogs2Excel.m
@@ -12,36 +12,36 @@ vIsFile = ~[svLogsInfo.isdir];
 svLogsInfo = svLogsInfo(vIsFile);
 
 Noise =     zeros(length(svLogsInfo), 1);
-Algorithm = cell(length(svLogsInfo), 1);
 Name =      cell(length(svLogsInfo), 1);
+Algorithm = cell(length(svLogsInfo), 1);
 Runtime =   zeros(length(svLogsInfo), 1);
 PSNR =      zeros(length(svLogsInfo), 1);
 SSIM =      zeros(length(svLogsInfo), 1);
 for iLog = 1:length(svLogsInfo)
     load(fullfile(logsDir, svLogsInfo(iLog).name), 'sLog');
     Noise(iLog) =     sLog.noiseStd;
-    Algorithm{iLog} = sLog.alg;
     Name{iLog} =      sLog.vidName;
+    Algorithm{iLog} = sLog.alg;
     Runtime(iLog) =   round(sLog.time*100)/100;
     PSNR(iLog) =      round(sLog.psnr*100)/100;
     SSIM(iLog) =      round(sLog.ssim*100)/100;
 end
 
 %% Create table:
-% sort - by noise, then alg, then name
-[~, vIndsNameSorted] = sort(Name);
-[~, vIndsAlgSorted] = sort(Algorithm(vIndsNameSorted));
-[~, vIndsNoiseSorted] = sort(Noise(vIndsNameSorted(vIndsAlgSorted)));
-vInds = vIndsNameSorted(vIndsAlgSorted(vIndsNoiseSorted));
+% sort - by noise, then name, then alg
+[~, vIndsAlgSorted] = sort(Algorithm);
+[~, vIndsNameSorted] = sort(Name(vIndsAlgSorted));
+[~, vIndsNoiseSorted] = sort(Noise(vIndsAlgSorted(vIndsNameSorted)));
+vInds = vIndsAlgSorted(vIndsNameSorted(vIndsNoiseSorted));
 
 Noise =     Noise(vInds);
-Algorithm = Algorithm(vInds);
 Name =      Name(vInds);
+Algorithm = Algorithm(vInds);
 Runtime =   Runtime(vInds);
 PSNR =      PSNR(vInds);
 SSIM =      SSIM(vInds);
 
-T = table(Noise, Algorithm, Name, PSNR, SSIM, Runtime);
+T = table(Noise, Name, Algorithm, PSNR, SSIM, Runtime);
 
 %% Write to excel:
 writetable(T, outFilename);
