a8b61f4542c619452b1244ad1b3a73d88c05c2b2
roy-hachnochi <roy.hachnochi@gmail.com>
2021-08-10 22:17:20 +0300

    bugFix: used noised video instead of original to calculate PSNR and SSIM, also added string as input type of paths in test for single file

--- RecreateResults.m
+++ RecreateResults.m
@@ -7,7 +7,7 @@
 vNoiseSigs = [10, 20, 40];
 vidNames =   {'gbicycle', 'gflower', 'gmissa', 'gsalesman', 'gstennis'};
 imNames =    {'lena'}; % just for testing WNNVD
-inDir =      fullfile('Videos','Noised');
+inDir =      'Videos';
 outDir =     fullfile('Results','Clean');
 logDir =     fullfile('Results','Logs');
 algs =       {'WNNVD', 'WNNID', 'VBM3D'};
@@ -23,29 +23,33 @@ sConfig = GetConfig();
 for noiseSig = vNoiseSigs
     for iAlg = 1:length(algs)
         if strcmp(algs{iAlg},'WNNVD') % run images only with WNNVD
-            inPaths =  cell(1, nInputs);
-            outPaths = cell(1, nInputs);
-            logPaths = cell(1, nInputs);
+            noisedPaths = cell(1, nInputs);
+            origPaths =   cell(1, nInputs);
+            outPaths =    cell(1, nInputs);
+            logPaths =    cell(1, nInputs);
         else
-            inPaths =  cell(1, nVids);
-            outPaths = cell(1, nVids);
-            logPaths = cell(1, nVids);
+            noisedPaths =  cell(1, nVids);
+            origPaths =    cell(1, nVids);
+            outPaths =     cell(1, nVids);
+            logPaths =     cell(1, nVids);
         end
         
         for iVid = 1:nVids
-            inPaths{iVid} = fullfile(inDir,[vidNames{iVid},'_',num2str(noiseSig),'.avi']);
+            noisedPaths{iVid} = fullfile(inDir,'Noised',[vidNames{iVid},'_',num2str(noiseSig),'.avi']);
+            origPaths{iVid} = fullfile(inDir,[vidNames{iVid},'.avi']);
             outPaths{iVid} = fullfile(outDir,[vidNames{iVid},'_',algs{iAlg},'_',num2str(noiseSig),'.avi']);
             logPaths{iVid} = fullfile(logDir,[vidNames{iVid},'_',algs{iAlg},'_',num2str(noiseSig)]);
         end
         
         if strcmp(algs{iAlg},'WNNVD') % run images only with WNNVD
             for iIm = 1:nIms
-                inPaths{nVids + iIm} = fullfile(inDir,[imNames{iIm},'_',num2str(noiseSig),'.png']);
+                noisedPaths{nVids + iIm} = fullfile(inDir,'Noised',[imNames{iIm},'_',num2str(noiseSig),'.png']);
+                origPaths{nVids + iIm} = fullfile(inDir,[imNames{iIm},'.png']);
                 outPaths{nVids + iIm} = fullfile(outDir,[imNames{iIm},'_',algs{iAlg},'_',num2str(noiseSig),'.png']);
                 logPaths{nVids + iIm} = fullfile(logDir,[imNames{iIm},'_',algs{iAlg},'_',num2str(noiseSig)]);
             end
         end
 
-        RunTest(sConfig, algs{iAlg}, inPaths, outPaths, logPaths, noiseSig, true, true);
+        RunTest(sConfig, algs{iAlg}, noisedPaths, origPaths, outPaths, logPaths, noiseSig, true);
     end
 end
--- RunTest.m
+++ RunTest.m
@@ -1,15 +1,15 @@
-function [] = RunTest(sConfig, alg, inPaths, outPaths, logPaths, noiseSig, isNoised, saveLog)
+function [] = RunTest(sConfig, alg, noisedPaths, origPaths, outPaths, logPaths, noiseSig, saveLog)
 % --------------------------------------------------------------------------------------------------------- %
 % Runs a video denoising test on a chosen video/set of videos.
 %
 % Input:
 %   sConfig -     Struct containing all parameters for algorithm (see GetConfig.m).
 %   alg -         (optional) Chosen video denoising algorithm ('WNNVD'/'WNNID'/'VBM3D', default: 'WNNVD').
-%   inPaths -     (optional) Input video paths (default: use path from sConfig).
+%   noisedPaths - (optional) Noised video paths (default: if not given, adds noise inside).
+%   origPaths -   (optional) Original video paths (default: use path from sConfig).
 %   outPaths -    (optional) Output video paths for saving (default: use path from sConfig).
 %   logPaths -    (optional) Log paths for saving (default: use path from sConfig).
 %   noiseSig -    (optional) Gaussian noise STD (default: use noiseSig from sConfig).
-%   isNoised -    (optional) Is input video noised, if not will add noise inside (default: true).
 %   saveLog -     (optional) Save log struct with run statistics (default: true).
 % --------------------------------------------------------------------------------------------------------- %
 
@@ -19,8 +19,8 @@ rng(42);
 if ~exist('alg', 'var') || isempty(alg)
     alg = 'WNNVD';
 end
-if ~exist('inPaths', 'var') || isempty(inPaths)
-    inPaths = {sConfig.sTest.vidInPath};
+if ~exist('origPaths', 'var') || isempty(origPaths)
+    origPaths = {sConfig.sTest.vidInPath};
 end
 if ~exist('outPaths', 'var') || isempty(outPaths)
     outPaths = {sConfig.sTest.vidOutPath};
@@ -31,21 +31,35 @@ end
 if exist('noiseSig', 'var') && ~isempty(noiseSig)
     sConfig.sNoise.sigma = noiseSig;
 end
-if ~exist('isNoised', 'var') || isempty(isNoised)
-    isNoised = true;
-end
 if ~exist('saveLog', 'var') || isempty(saveLog)
     saveLog = true;
 end
+noisedExists = (exist('noisedPaths', 'var') && ~isempty(noisedPaths));
+
+if ischar(noisedPaths) || isstring(noisedPaths)
+    noisedPaths = {char(noisedPaths)};
+end
+if ischar(origPaths) || isstring(origPaths)
+    origPaths = {char(origPaths)};
+end
+if ischar(outPaths) || isstring(outPaths)
+    outPaths = {char(outPaths)};
+end
+if ischar(logPaths) || isstring(logPaths)
+    logPaths = {char(logPaths)};
+end
 
 %% Argument checks:
-nVids = length(inPaths);
+nVids = length(origPaths);
 assert(ismember(alg, {'WNNVD', 'VBM3D', 'WNNID'}), 'Illegal alg.');
 assert(length(outPaths) == nVids,...
     'Unmatching number of input/output paths.')
 assert(sConfig.sNoise.sigma > 0, 'Illegal noise STD.');
 if saveLog
     assert(length(logPaths) == nVids, 'Unmatching number of input/log paths.')
+end
+if noisedExists
+    assert(length(noisedPaths) == nVids, 'Unmatching number of input/noised paths.');
 end
 
 %% Prepare parameters for algs:
@@ -56,19 +70,23 @@ end
 %% Run video denoising algorithm on all vids:
 for iVid = 1:nVids
     %% Load video:
-    [mOrigVid, frameRate] = LoadVideo(inPaths{iVid}, sConfig.sTest);
+    [mOrigVid, frameRate] = LoadVideo(origPaths{iVid}, sConfig.sTest);
+    if noisedExists
+        [mNoised, frameRate] = LoadVideo(noisedPaths{iVid}, sConfig.sTest);
+        assert(all(size(mNoised) == size(mOrigVid)), 'Noised and original videos must be of same size.');
+    end
     [h, w, ch, f] = size(mOrigVid);
     
-    vidName = split(inPaths{iVid}, filesep); % split from directory
+    vidName = split(origPaths{iVid}, filesep); % split from directory
     vidName = split(vidName{end}, '.'); % split from extension
     vidName = split(vidName{1}, '_'); % split from additions if exist
     vidName = vidName{1};
 
     %% Add noise:
-    if ~isNoised
-        mX = VideoNoise(mOrigVid, sConfig.sNoise);
+    if noisedExists
+        mX = mNoised;
     else
-        mX = mOrigVid;
+        mX = VideoNoise(mOrigVid, sConfig.sNoise);
     end
 
     %% Denoise:
@@ -90,7 +108,7 @@ for iVid = 1:nVids
     sLog.psnr = PSNR(mOrigVid, mY);
     sLog.ssim = SSIM(mOrigVid, mY);
     fprintf("\n==== Finished %d/%d videos | PSNR: %.2f | SSIM: %.2f | Time: %.2f min ====\n",...
-        iVid, nVids, sLog.psnr, sLog.ssim, sLog.time/60);
+        iVid, nVids, sLog.psnr, sLog.ssim, sLog.timePerFrame*f/60);
 
     %% Save results:
     sLog.alg = alg;
